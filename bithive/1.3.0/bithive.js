2385a36bff27927c6925bf4e7540caa910340dc1
ariel <ab@abcontenidos.com>
2021-05-11 14:10:56 -0300

    swtich

--- bithive/1.3.0/bithive.js
+++ bithive/1.3.0/bithive.js
@@ -3065,6 +3065,7 @@ jQuery.extend({
 							;
 
 							if(args[0].data("boolean")=="1" || args[0].data("boolean")=="true") {
+								console.debug(args[1][0]);
 								if(args[1][0]!=0 && args[1][0]!="0" && args[1][0]!="") { $("input", onoff).prop("checked", true); }
 							} else {
 								if($.inArray($("input", onoff).val(), args[1])>=0) { $("input", onoff).prop("checked", true); }
