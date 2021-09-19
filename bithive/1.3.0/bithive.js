91fa981a65693c2a1859596bf10de9e9681ee030
Ariel Bottero <ab@abcontenidos.com>
2021-09-19 16:57:02 -0300

    bithive

--- bithive/1.3.0/bithive.js
+++ bithive/1.3.0/bithive.js
@@ -3479,7 +3479,7 @@ jQuery.extend({
 		},
 
 		help: function(doc) {
-			$.bithive.rightbar.load("https://raw.githubusercontent.com/hytcom/wiki/master/abiz/"+doc+".md?hash="+$.uid(), true);
+			$.bithive.rightbar.load("https://wiki.hytcom.net/raw/abiz/"+doc+".md?hash="+$.uid(), true);
 		},
 
 		print: function() {
