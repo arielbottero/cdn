e2a0650aef6aa858f34ae6d5f7c85f19b1000e7c
Ariel Bottero <ab@abcontenidos.com>
2022-04-25 17:01:09 -0300

    ebloock

--- bithive/1.3.0/bithive.js
+++ bithive/1.3.0/bithive.js
@@ -3453,7 +3453,7 @@ jQuery.extend({
 				} else if(typeof source=="object" && !!source) {
 					clearInterval(loader);
 					func(source, args);
-				} else if(limit==500) {
+				} else if(limit==200) {
 					console.log("Request timeout: "+src);
 					clearInterval(loader);
 				}
